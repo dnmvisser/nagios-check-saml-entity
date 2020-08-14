3730d713848043cb6adb11b1e58e04c119e73945
Dick Visser <dick.visser@geant.org>
2020-08-14 10:49:04 +0200

    Disable hostname verification

--- nagios-check-saml-entity.py
+++ nagios-check-saml-entity.py
@@ -62,6 +62,8 @@ try:
 
     hostname = urlparse(acs_url).hostname
     context = ssl.create_default_context()
+    context.check_hostname = False
+    context.verify_mode = ssl.CERT_NONE
     with socket.create_connection((hostname, 443)) as sock:
         with context.wrap_socket(sock, server_hostname = hostname) as tls_sock:
             cert = tls_sock.getpeercert()
