493f1e7923322f1f7d3b196d0f45f0ca542d6509
Dick Visser <dick.visser@geant.org>
2021-01-29 13:48:46 +0100

    Add option to check SAML cert expiration

--- nagios-check-saml-entity.py
+++ nagios-check-saml-entity.py
@@ -11,7 +11,8 @@ import ssl
 import socket
 from pprint import pprint
 from datetime import datetime
-
+from cryptography import x509
+import base64
 
 # Debug
 # import logging
@@ -25,20 +26,31 @@ try:
     parser = argparse.ArgumentParser(
             description='Check various properties of a SAML entity'
             )
+
     parser.add_argument('--entity',
             help='the entityID to check',
             required=True
             )
    
-    # https://github.com/iay/md-query/blob/master/draft-young-md-query.txt#L362
-    parser.add_argument('--mdq',
-            help='The base URL of the MDQ responder',
-            required=True
+    group1 = parser.add_mutually_exclusive_group()
+    group1.add_argument('--location',
+            help='The location of the metadata file. Can be a path or a URL. ' +
+                'Mutually exclusive with the --mdq option',
             )
-    parser.add_argument('--warning',
-            help='Minimum number of days a certificate has to be valid to issue a WARNING',
+    # https://github.com/iay/md-query/blob/master/draft-young-md-query.txt#L362
+    group1.add_argument('--mdq',
+            help='The base URL of the MDQ responder. Mutually exclusive with ' +
+                'the --location option',
+            )
+
+    parser.add_argument('--tls_cert_days',
+            help='Minimum number of days the TLS certificate of the SAML ' +
+                'Assertion Consumer URL has to be valid',
             type=int,
-            default=21
+            )
+    parser.add_argument('--saml_cert_days',
+            help='Minimum number of days a SAML certificate has to be valid',
+            type=int,
             )
 
     
@@ -50,39 +62,79 @@ try:
     warn_msg = []
     crit_msg = []
 
+    # pprint(args)
 
-    url = "{base}/entities/{endpoint}".format(
-        base=args.mdq,
-        endpoint=MetaDataMDX.sha1_entity_transform(args.entity),
-    )
     mds = MetadataStore(attrc=None, config=Config())
-    mds.load("remote", url=url)
-    res = mds.assertion_consumer_service(entity_id=args.entity)
-    acs_url = next(iter(res), {}).get("location")
 
-    hostname = urlparse(acs_url).hostname
-    context = ssl.create_default_context()
-    with socket.create_connection((hostname, 443)) as sock:
-        with context.wrap_socket(sock, server_hostname = hostname) as tls_sock:
-            cert = tls_sock.getpeercert()
-            # pprint(cert)
-            if 'notAfter' in cert:
-                expire_date = datetime.strptime(cert['notAfter'],
-                        "%b %d %H:%M:%S %Y %Z")
-                expire_in = expire_date - datetime.now()
 
-                if expire_in.days < 0:
-                    crit_msg.append("X.509 certificate '" + hostname +
-                            "' expired on " + cert['notAfter'] +
-                            " (" + str(abs(expire_in.days)) + " days ago)")
-                elif expire_in.days < args.warning:
-                    warn_msg.append("X.509 certificate '" + hostname +
-                            "' is valid until " + cert['notAfter'] +
-                            " (expires in " + str(expire_in.days) + " days)")
-                else:
-                    ok_msg.append("X.509 certificate '" + hostname +
-                            "' is valid until " + cert['notAfter'] +
-                            " (expires in " + str(expire_in.days) + " days)")
+    if args.mdq is not None:
+        url = "{base}/entities/{endpoint}".format(
+            base=args.mdq,
+            endpoint=MetaDataMDX.sha1_entity_transform(args.entity),
+        )
+    if urlparse(args.location).scheme in ['http', 'https']:
+        url=args.location
+        mds.load("remote", url=url)
+    else:
+        mds.load("local", args.location)
+
+
+    # Expiration check on the TLS certificate of the SAML ACS URL
+    if args.tls_cert_days is not None:
+        acs_res = mds.assertion_consumer_service(entity_id=args.entity)
+        acs_url = next(iter(acs_res), {}).get("location")
+        hostname = urlparse(acs_url).hostname
+        if urlparse(acs_url).scheme == 'https':
+            context = ssl.create_default_context()
+            with socket.create_connection((hostname, 443)) as sock:
+                with context.wrap_socket(sock, server_hostname = hostname) as tls_sock:
+                    cert = tls_sock.getpeercert()
+                    # pprint(cert)
+                    if 'notAfter' in cert:
+                        expire_date = datetime.strptime(cert['notAfter'],
+                                "%b %d %H:%M:%S %Y %Z")
+                        expire_in = expire_date - datetime.now()
+
+                        if expire_in.days < 0:
+                            crit_msg.append("TLS certificate for '" + hostname +
+                                    "' expired on " + cert['notAfter'] +
+                                    " (" + str(abs(expire_in.days)) + " days ago)")
+                        elif expire_in.days < args.tls_cert_days:
+                            warn_msg.append("TLS certificate for '" + hostname +
+                                    "' is valid until " + cert['notAfter'] +
+                                    " (expires in " + str(expire_in.days) + " days)")
+                        else:
+                            ok_msg.append("TLS certificate for '" + hostname +
+                                    "' is valid until " + cert['notAfter'] +
+                                    " (expires in " + str(expire_in.days) + " days)")
+        else:
+            warn_msg.append("Non-HTTPS Assertion Consumer Service URL: " + acs_url)
+
+    if args.saml_cert_days is not None:
+        certs = list(set(
+            mds.certs(entity_id=args.entity, descriptor='any', use='encryption') +
+            mds.certs(entity_id=args.entity, descriptor='any', use='signing')))
+        if len(certs) > 0:
+            for i in certs:
+                cert = x509.load_der_x509_certificate(base64.b64decode(i))
+                if cert.not_valid_after:
+                    expire_in = cert.not_valid_after - datetime.now()
+
+                    if expire_in.days < 0:
+                        crit_msg.append("A SAML certificate expired on " +
+                                cert.not_valid_after.ctime() +
+                                " (" + str(abs(expire_in.days)) + " days ago)")
+                    elif expire_in.days < args.saml_cert_days:
+                        warn_msg.append("A SAML certificate is valid until " +
+                                cert.not_valid_after.ctime() +
+                                " (expires in " + str(expire_in.days) + " days)")
+                    else:
+                        ok_msg.append("A SAML certificate is valid until " +
+                                cert.not_valid_after.ctime() +
+                                " (expires in " + str(expire_in.days) + " days)")
+        else:
+            ok_msg.append("No SAML certificates found in metatada for entity " + args.entity)
+
 
 except Exception as e:
     # pprint(e)
